0c0fcd3934551f8dfd6a271873d75ed9129ea221
riccardomodanese <riccardo.modanese@eurotech.com>
2017-06-30 11:00:59 +0200

    connection-user coupling - broker side enforcement implementation
    Signed-off-by: riccardomodanese <riccardo.modanese@eurotech.com>

--- service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/connection/DeviceConnectionCreator.java
+++ service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/connection/DeviceConnectionCreator.java
@@ -11,6 +11,8 @@
  *******************************************************************************/
 package org.eclipse.kapua.service.device.registry.connection;
 
+import javax.xml.bind.annotation.XmlElement;
+
 import org.eclipse.kapua.model.KapuaUpdatableEntityCreator;
 import org.eclipse.kapua.model.id.KapuaId;
 import org.eclipse.kapua.service.device.registry.ConnectionUserCouplingMode;
@@ -77,6 +79,21 @@ public interface DeviceConnectionCreator extends KapuaUpdatableEntityCreator<Dev
      * @param reservedUserId
      */
     public void setReservedUserId(KapuaId reservedUserId);
+
+    /**
+     * Gets whether or not the {@link DeviceConnection} can change user on the next login.
+     * 
+     * @return <code>true</code> if device can changhe user to connect, <code>false</code> if not.
+     */
+    @XmlElement(name = "allowUserChange")
+    public boolean getAllowUserChange();
+
+    /**
+     * Sets whether or not the {@link DeviceConnection} can change user on the next login.
+     * 
+     * @param allowUserChange
+     */
+    public void setAllowUserChange(boolean allowUserChange);
 
     /**
      * Get the device protocol
